61d81c6310dbed2562a0a5b3497420b56764eb5a
Jim-Kristian Malmhaug <Jim-Kristian Malmhaug>
2017-02-27 22:32:51 +0100

    Please enter the commit message for your changes. Lines starting

---
+++ Chapter 4/C4T2_Fahrenheit2Celsius/C4T2_Fahrenheit2Celsius/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace C4T2_Fahrenheit2Celsius
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.celsiusBox = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.fahrenheitBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // celsiusBox
+            // 
+            this.celsiusBox.Location = new System.Drawing.Point(15, 69);
+            this.celsiusBox.Name = "celsiusBox";
+            this.celsiusBox.Size = new System.Drawing.Size(100, 20);
+            this.celsiusBox.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // fahrenheitBox
+            // 
+            this.fahrenheitBox.Enabled = false;
+            this.fahrenheitBox.Location = new System.Drawing.Point(244, 69);
+            this.fahrenheitBox.Name = "fahrenheitBox";
+            this.fahrenheitBox.Size = new System.Drawing.Size(100, 20);
+            this.fahrenheitBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Celsius";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(241, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Fahrenheit";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(97, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(177, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Temperature Converter";
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(142, 67);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(75, 23);
+            this.convertButton.TabIndex = 6;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 134);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fahrenheitBox);
+            this.Controls.Add(this.celsiusBox);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Fahrenheit to Celsisus Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox celsiusBox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox fahrenheitBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button convertButton;
+    }
+}
+
---
+++ Chapter 4/C4T2_Fahrenheit2Celsius/C4T2_Fahrenheit2Celsius/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C4T2_Fahrenheit2Celsius
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            double fahrenheit = (Double.Parse(celsiusBox.Text) * 1.8) + 32; //Calculate fahrenheit
+            fahrenheitBox.Text = fahrenheit.ToString();
+        }
+    }
+}

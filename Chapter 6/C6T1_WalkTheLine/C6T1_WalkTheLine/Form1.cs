7f7a70f1f23e6e72a57a597359afe47ecca12748
Jim-Kristian Malmhaug <malmhaug@gmail.com>
2017-05-23 21:19:51 +0200

    Add Chapter 6 and task 1

---
+++ Chapter 6/C6T1_WalkTheLine/C6T1_WalkTheLine/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C6T1_WalkTheLine
+{
+    public partial class Form1 : Form
+    {
+        Point[] points =
+        {
+            new Point(400,25),
+            new Point(100,15),
+            new Point(200,70),
+            new Point(700,100),
+            new Point(900,75)
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Paint += new PaintEventHandler(Paint_String);
+        }
+
+        private void Paint_String(object sender, PaintEventArgs e)
+        {
+            //create a pen
+            Pen pen = new Pen(Color.Blue);
+            //draw the path
+            e.Graphics.DrawLines(pen, points);
+            pen.Dispose();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Chapter 6/PingPong/PingPong/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingPong
+{
+    public partial class Form1 : Form
+    {
+        int paddle_x = 0;
+        int paddle_y = 255;
+        int paddle_width = 35;
+        int paddle_height = 20;
+        Graphics g;
+        int x = 0; //initial x posistion of the ball
+        int y = 0; //initial y posistion of the ball
+        int dx = 8; //position increment for the ball on the x axis
+        int dy = 5; //position increment for the ball on the y axis
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.MouseMove += new MouseEventHandler(MovePaddle);
+            this.Paint += new PaintEventHandler(paint1);
+            Console.Write("Initialization...");
+        }
+
+        private void paint1(Object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+            //draw the paddle
+            SolidBrush blueBrush = new SolidBrush(Color.Blue);
+            g.FillRectangle(blueBrush, paddle_x, paddle_y, paddle_width, paddle_height);
+
+            //draw the ball
+            SolidBrush brush = new SolidBrush(Color.Blue);
+            g.FillEllipse(brush, x, y, 10, 10);
+        }
+
+        private void MovePaddle(Object sender, MouseEventArgs e)
+        {
+            paddle_y = e.Y;
+            paddle_x = e.X;
+            Invalidate();
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            MoveBall();
+        }
+
+        private void MoveBall()
+        {
+            int newBall_x = x + dx;
+            int newBall_y = y + dy;
+
+            //Bounce the ball if it has collided with a wall
+            if ((newBall_x < 0) || (newBall_x > 300)) dx = -dx;
+            if ((newBall_y < 0) || (newBall_y > 255)) dy = -dy;
+
+            //Bounce the ball if it has collided with the paddle
+            if (((newBall_x > paddle_x) && (newBall_x < (paddle_x + paddle_width))) && ((newBall_y > paddle_y) && (newBall_y < (paddle_y + paddle_height))))
+            {
+                if (dx > 0 && dy > 0)
+                {
+                    dy = -dy;
+                }
+                if (dx < 0 && dy > 0)
+                {
+                    dy = -dy;
+                }
+            }
+            x = x + dx;
+            y = y + dy;
+            Invalidate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
